ccaae9b2016a8e504a7834a1e1234128cf8fdda8
savKor <savvatejkorovin@gmail.com>
2022-07-08 16:35:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <Header></Header>
+    <v-main>
+      <HelloWorld></HelloWorld>
+    </v-main>
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    Header,
+    Footer
+},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
